8d00c6a6fd2d956141c3e1545702681cc2d69b09
CrashStatic <thecrashstatic@gmail.com>
2025-02-06 19:33:56 +0300

    Удаляет лишнюю типизацию констант

--- source/scripts/modules/constants.ts
+++ source/scripts/modules/constants.ts
@@ -1,50 +1,50 @@
-export const BODY_SELECTOR: string = '.body';
+export const BODY_SELECTOR = '.body';
 
-export const LETTER_TEMPLATE_SELECTOR: string = '#letter';
-export const MESSAGE_TEMPLATE_SELECTOR: string = '#message';
-export const MODAL_TEMPLATE_SELECTOR: string = '#search-modal-content';
+export const LETTER_TEMPLATE_SELECTOR = '#letter';
+export const MESSAGE_TEMPLATE_SELECTOR = '#message';
+export const MODAL_TEMPLATE_SELECTOR = '#search-modal-content';
 
-export const ELEMENT_SELECTOR: string = '.element';
-export const ELEMENT_ACTIVE_SELECTOR: string = 'element--active';
-export const LETTER_SELECTOR: string = '.element__letter';
-export const CONTACTS_SELECTOR: string = '.element__contacts';
-export const CONTACTS_OPEN_SELECTOR: string = 'element__contacts--open';
-export const COUNTER_SELECTOR: string = '.element__counter';
-export const COUNTER_ACTIVE_SELECTOR: string = 'element__counter--active';
-export const ELEMENT_MESSAGE_SELECTOR: string = '.element__message';
+export const ELEMENT_SELECTOR = '.element';
+export const ELEMENT_ACTIVE_SELECTOR = 'element--active';
+export const LETTER_SELECTOR = '.element__letter';
+export const CONTACTS_SELECTOR = '.element__contacts';
+export const CONTACTS_OPEN_SELECTOR = 'element__contacts--open';
+export const COUNTER_SELECTOR = '.element__counter';
+export const COUNTER_ACTIVE_SELECTOR = 'element__counter--active';
+export const ELEMENT_MESSAGE_SELECTOR = '.element__message';
 
-export const COLUMN_ELEMENT_SELECTOR: string = '.column__element';
+export const COLUMN_ELEMENT_SELECTOR = '.column__element';
 
-export const MESSAGE_SELECTOR: string = '.message';
-export const MESSAGE_NAME_SELECTOR: string = '.message__name';
-export const MESSAGE_POSITION_SELECTOR: string = '.message__position';
-export const MESSAGE_PHONE_SELECTOR: string = '.message__phone';
+export const MESSAGE_SELECTOR = '.message';
+export const MESSAGE_NAME_SELECTOR = '.message__name';
+export const MESSAGE_POSITION_SELECTOR = '.message__position';
+export const MESSAGE_PHONE_SELECTOR = '.message__phone';
 
-export const CONTACT_TABLE: string = '.contact-table';
-export const CONTACT_DELETE_BTN: string = '.js-delete-contact-button';
-export const CONTACT_EDIT_BTN: string = '.js-edit-contact-button';
-export const CONTACT_CLEAR_BTN: string = '.js-clear-contact-button';
-export const CONTACT_SEARCH_BTN: string = '.js-search-contact-button';
+export const CONTACT_TABLE = '.contact-table';
+export const CONTACT_DELETE_BTN = '.js-delete-contact-button';
+export const CONTACT_EDIT_BTN = '.js-edit-contact-button';
+export const CONTACT_CLEAR_BTN = '.js-clear-contact-button';
+export const CONTACT_SEARCH_BTN = '.js-search-contact-button';
 
-export const EDIT_POPUP: string = '#edit-popup-content';
-export const EDIT_POPUP_NAME: string = '#popup-name';
-export const EDIT_POPUP_POSITION: string = '#popup-position';
-export const EDIT_POPUP_PHONE: string = '#popup-phone';
-export const EDIT_POPUP_ERROR: string = '.popup__error';
+export const EDIT_POPUP = '#edit-popup-content';
+export const EDIT_POPUP_NAME = '#popup-name';
+export const EDIT_POPUP_POSITION = '#popup-position';
+export const EDIT_POPUP_PHONE = '#popup-phone';
+export const EDIT_POPUP_ERROR = '.popup__error';
 
-export const MODAL_SELECTOR: string = '.modal';
-export const MODAL_TITLE: string = '.modal__title';
-export const MODAL_BODY: string = '.modal__body';
-export const MODAL_INPUT: string = '.modal__input';
-export const MODAL_SEARCH_AREA: string = '.modal__search-area';
-export const MODAL_SHOW_BTN: string = '.modal__button-show';
-export const MODAL_CLOSE_BTN: string = '.modal__close-button';
-export const MODAL_OVERLAY: string = '.modal__overlay';
+export const MODAL_SELECTOR = '.modal';
+export const MODAL_TITLE = '.modal__title';
+export const MODAL_BODY = '.modal__body';
+export const MODAL_INPUT = '.modal__input';
+export const MODAL_SEARCH_AREA = '.modal__search-area';
+export const MODAL_SHOW_BTN = '.modal__button-show';
+export const MODAL_CLOSE_BTN = '.modal__close-button';
+export const MODAL_OVERLAY = '.modal__overlay';
 
-export const FORM_SELECTOR: string = '.form';
-export const FORM_NAME_ID: string = 'name';
-export const FORM_POSITION_ID: string = 'position';
-export const FORM_PHONE_ID: string = 'phone';
-export const FORM_ERROR: string = '.form__error';
-export const FORM_BTNS: string = '.form__buttons';
-export const FORM_BTN_SAVE: string = '.form__button--popup-save';
+export const FORM_SELECTOR = '.form';
+export const FORM_NAME_ID = 'name';
+export const FORM_POSITION_ID = 'position';
+export const FORM_PHONE_ID = 'phone';
+export const FORM_ERROR = '.form__error';
+export const FORM_BTNS = '.form__buttons';
+export const FORM_BTN_SAVE = '.form__button--popup-save';
